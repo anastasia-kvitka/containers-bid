98ab250e635a05621874becbe64ee5e29775f20c
Anastasia Kvitka <nasti.carrot@gmail.com>
2025-11-03 14:13:33 +0200

    Quick fix

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
     setStatus("loading");
 
     try {
-      const res = await fetch("/api/send-email", {
+      const res = await fetch("/api/send", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, link }),
